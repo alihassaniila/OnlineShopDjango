62b6d046b3cca3927dc387a71d9b6a0b64757552
Ali Hassani <hasaniradali@gmail.com>
2023-09-06 21:54:53 +0330

    add users app

--- products/views.py
+++ products/views.py
@@ -5,6 +5,25 @@ from rest_framework import status
 from .models import Category, Product ,File
 from .serializers import CategorySerializer, ProductSerializer ,FileSerializer
 
+class CategoryListView(APIView):
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class CategoryDetailView(APIView):
+
+    def get(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CategorySerializer(category, context={'request': request})
+        return Response(serializer.data)
+
 class ProductListView(APIView):
 
     def get(self, request):
@@ -21,4 +40,22 @@ class ProductDetailView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductSerializer(product, context={'request': request})
+        return Response(serializer.data)
+
+class FileListView(APIView):
+
+    def get(self, request, product_id):
+        files = File.objects.filter(product_id=product_id)
+        serializer = FileSerializer(files, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class FileDetailView(APIView):
+
+    def get(self, request, product_id, pk):
+        try:
+            f = File.objects.get(pk=pk, product_id=product_id)
+        except File.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = FileSerializer(f, context={'request': request})
         return Response(serializer.data)
